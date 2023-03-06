6e5ad8d29ea5f3bf4f85d0e4f06edb3ce02c93d5
Mohamed-Ashraaaf <mohamed.ashraaaf@gmail.com>
2023-03-06 12:35:24 -0800

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,26 @@
 #include "lists.h"
+
 /**
-* check_cycle - function that checks if list has cycle
-* @list: pointer to list to check
-* Return: On success 1
-* On error, -1 and errno
-*/
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
-	{
 		return (0);
-	}
 
-	while (list)
+	while (slow && fast && fast->next)
 	{
-		temp = list;
-		list = list->next;
-
-		if (temp <= list)
-		{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-		}
 	}
+
 	return (0);
 }
