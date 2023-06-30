1fa7cd4c48aef44c11c765ca66de6e81f2df6b02
Mohamed-Ashraaaf <mohamed.ashraaaf@gmail.com>
2023-06-30 15:35:39 -0700

    some fixes

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that causes the server to respond with a message containing You got me!
+# Script that causes the server to respond with a message You got me!
 curl -sX POST 0.0.0.0:5000/catch_me
