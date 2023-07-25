fc7b5947c7818402bd8455219ae4d8ba8a2e0c14
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-07-25 14:47:42 +0000

    some fixes

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,35 +1,31 @@
 #!/usr/bin/node
-
 const request = require('request');
 
 const apiUrl = process.argv[2];
 
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = {};
-
-      todos.forEach((todo) => {
-        if (todo.completed) {
-          const userId = todo.userId;
-          if (completedTasksByUser[userId]) {
-            completedTasksByUser[userId]++;
-          } else {
-            completedTasksByUser[userId] = 1;
-          }
-        }
-      });
-
-      console.log('{');
-      for (const userId in completedTasksByUser) {
-        console.log(`'${userId}': ${completedTasksByUser[userId]}`);
-      }
-      console.log('}');
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+
+  const completedTasks = tasks.reduce((acc, task) => {
+    if (task.completed) {
+      if (acc[task.userId]) {
+        acc[task.userId] += 1;
+      } else {
+        acc[task.userId] = 1;
+      }
+    }
+    return acc;
+  }, {});
+
+  console.log(completedTasks);
 });
